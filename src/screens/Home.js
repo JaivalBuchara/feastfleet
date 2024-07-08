54b6e92430d62a86a25bd71b27636735768615e2
JaivalBuchara <jaival.buchara2021@vitstudent.ac.in>
2024-07-08 10:02:44 +0530

    initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Navbar from '../components/navbar';
+export default function Home() {
+    return (
+        <div>
+        <div><Navbar/></div>
+        <div>Body</div>
+        <div><Footer/></div>
+        </div>
+      );
+}
